1e91b7bfab984d86be07a18278de0ea6835e1b9c
hsas2025 <hsas2025@outlook.com>
2025-03-02 07:57:04 +0800

    update

--- AlphaZero_train.py
+++ AlphaZero_train.py
@@ -1,3 +1,8 @@
+import subprocess
+import time
+import pickle
+import sys
+
 import numpy as np
 import random
 from collections import deque
@@ -19,7 +24,7 @@ healpower = 1
 isDiagHeal = True
 isDiagAttack = True
 
-class Game:  
+class Game:
     def __init__(self):
         self.board = np.array([[{'type': EMPTY, 'health': 0} for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)])
         self.currentPlayer = PLAYER_X
@@ -197,22 +202,22 @@ class Game:
 
 class MCTSNode:
     def __init__(self, state, parent=None, prior_prob=0):
-        self.state = state  
-        self.parent = parent  
-        self.children = {}  
-        self.visit_count = 0  
-        self.total_value = 0  
-        self.prior_prob = prior_prob  
+        self.state = state
+        self.parent = parent
+        self.children = {}
+        self.visit_count = 0
+        self.total_value = 0
+        self.prior_prob = prior_prob
         self.u = 0
 
     def is_fully_expanded(self):
-        
+
         return len(self.children) == len(self.get_legal_actions())
 
     def get_legal_actions(self):
-        
+
         game = Game()
-        
+
         game.board = np.empty((BOARD_SIZE, BOARD_SIZE), dtype=object)
         for i in range(BOARD_SIZE):
             for j in range(BOARD_SIZE):
@@ -227,7 +232,7 @@ class MCTSNode:
                     game.board[i][j]['type'] = PLAYER_O
                     game.board[i][j]['health'] = inithealth
                 elif self.state[state_index] == 0:
-                    
+
                     is_block = True
                     directNeighbors = [(-1, 0), (0, -1), (0, 1), (1, 0)]
                     for dr, dc in directNeighbors:
@@ -248,7 +253,7 @@ class MCTSNode:
         return game.get_valid_moves()
 
     def select_child(self, c_puct=1.0):
-        
+
         best_child = None
         best_ucb = -float('inf')
 
@@ -257,7 +262,7 @@ class MCTSNode:
             return None
 
         for action, child in children_list:
-            
+
             ucb = child.total_value / (child.visit_count + 1e-8) + \
                   c_puct * child.prior_prob * math.sqrt(self.visit_count) / (1 + child.visit_count)
             child.u = ucb
@@ -267,16 +272,16 @@ class MCTSNode:
                 best_child = child
 
         if best_child is None:
-            
+
             return random.choice(children_list)[1]
-        
+
         return best_child
 
     def expand(self, action_probs):
-        
+
         for action, prob in action_probs.items():
             if action not in self.children:
-                
+
                 game = Game()
                 game.board = np.empty((BOARD_SIZE, BOARD_SIZE), dtype=object)
                 for i in range(BOARD_SIZE):
@@ -292,7 +297,7 @@ class MCTSNode:
                             game.board[i][j]['type'] = PLAYER_O
                             game.board[i][j]['health'] = inithealth
                         elif self.state[state_index] == 0:
-                            
+
                             is_block = True
                             directNeighbors = [(-1, 0), (0, -1), (0, 1), (1, 0)]
                             for dr, dc in directNeighbors:
@@ -313,11 +318,11 @@ class MCTSNode:
                 game.make_move(action[0], action[1])
                 new_state = game.get_state()
 
-                
+
                 self.children[action] = MCTSNode(new_state, parent=self, prior_prob=prob)
 
     def update(self, value):
-        
+
         self.visit_count += 1
         self.total_value += value
         if self.parent:
@@ -332,18 +337,18 @@ class AlphaZero:
         self.learning_rate = learning_rate
         self.c_puct = c_puct
         self.num_simulations = num_simulations
-        self.temperature = temperature if temperature > 0 else 1e-8 
+        self.temperature = temperature if temperature > 0 else 1e-8
         self.model = self._build_model() if model_path is None else self.load_model(model_path)
 
     def _build_model(self):
-        
+
         input_layer = Input(shape=(BOARD_SIZE * BOARD_SIZE,))
-        
-        
+
+
         policy_hidden = Dense(64, activation='relu')(input_layer)
         policy_output = Dense(BOARD_SIZE * BOARD_SIZE, activation='softmax', name='policy')(policy_hidden)
 
-        
+
         value_hidden = Dense(32, activation='relu')(input_layer)
         value_output = Dense(1, activation='tanh', name='value')(value_hidden)
 
@@ -353,43 +358,43 @@ class AlphaZero:
         return model
 
     def save_model(self, filepath):
-        
+
         self.model.save(filepath)
 
     def load_model(self, filepath):
-        
+
         return load_model(filepath)
 
     def mcts(self, root_state):
-        
+
         root = MCTSNode(root_state)
 
         for _ in range(self.num_simulations):
             node = root
             search_path = [node]
 
-            
+
             while node is not None and node.is_fully_expanded():
                 node = node.select_child(self.c_puct)
                 if node is not None:
                     search_path.append(node)
 
-            
+
             if node is None:
                 value = self.expand_and_evaluate(search_path[-1])
             else:
                 value = self.expand_and_evaluate(node)
 
-            
+
             for node in reversed(search_path):
                 node.update(value)
 
         return root
 
     def expand_and_evaluate(self, node):
-      
+
       game = Game()
-      
+
       game.board = np.empty((BOARD_SIZE, BOARD_SIZE), dtype=object)
       for i in range(BOARD_SIZE):
           for j in range(BOARD_SIZE):
@@ -404,7 +409,7 @@ class AlphaZero:
                   game.board[i][j]['type'] = PLAYER_O
                   game.board[i][j]['health'] = inithealth
               elif node.state[state_index] == 0:
-                  
+
                   is_block = True
                   directNeighbors = [(-1, 0), (0, -1), (0, 1), (1, 0)]
                   for dr, dc in directNeighbors:
@@ -421,91 +426,91 @@ class AlphaZero:
                       game.board[i][j]['type'] = EMPTY
                       game.board[i][j]['health'] = 0
               state_index += 1
-      
+
       game.currentPlayer = PLAYER_X if node.state.sum() >= 0 else PLAYER_O
       winner = game.determine_winner()
 
       if winner is not None:
-          
+
           value = 1 if winner == game.currentPlayer else -1
           return value
 
-      
+
       policy, value = self.model.predict(node.state.reshape(1, -1), verbose=0)
       policy = policy[0]
       value = value[0][0]
 
-      
+
       legal_actions = node.get_legal_actions()
       action_probs = {action: policy[action[0] * BOARD_SIZE + action[1]] for action in legal_actions}
 
-      
+
       node.expand(action_probs)
 
       return value
 
       if winner is not None:
-          
+
           value = 1 if winner == game.currentPlayer else -1
           return value
 
-      
+
       policy, value = self.model.predict(node.state.reshape(1, -1), verbose=0)
       policy = policy[0]
       value = value[0][0]
 
-      
+
       legal_actions = node.get_legal_actions()
       action_probs = {action: policy[action[0] * BOARD_SIZE + action[1]] for action in legal_actions}
 
-      
+
       node.expand(action_probs)
 
       return value
 
     def get_action_probs(self, state):
-        
+
         root = self.mcts(state)
         game1 = Game()
-        
-        
-        
 
-        
+
+
+
+
         visit_counts = np.array([root.children.get(action, MCTSNode(None)).visit_count for action in [(i,j) for i in range(BOARD_SIZE) for j in range(BOARD_SIZE)]])
-        
+
         if self.temperature == 0:
-            
+
             action_probs = np.zeros(BOARD_SIZE * BOARD_SIZE)
             action_probs[np.argmax(visit_counts)] = 1
         else:
-            
-            
+
+
             safe_visit_counts = visit_counts + 1e-8
             safe_visit_counts = safe_visit_counts ** (1 / self.temperature)
             action_probs = safe_visit_counts / np.sum(safe_visit_counts)
-        
-        
+
+
         action_probs = action_probs.reshape(BOARD_SIZE, BOARD_SIZE)
 
-        
+
         valid_moves = game1.get_valid_moves()
-        
+
         masked_probs = np.zeros((BOARD_SIZE, BOARD_SIZE))
 
         if not valid_moves:
-            
+
             return masked_probs
 
         for move in valid_moves:
             masked_probs[move[0]][move[1]] = action_probs[move[0]][move[1]]
-        
-        
+
+
         sum_probs = np.sum(masked_probs)
         if sum_probs > 0:
             masked_probs /= sum_probs
         else:
-            
+
             if valid_moves:
                 prob = 1.0 / len(valid_moves)
                 for move in valid_moves:
@@ -516,7 +521,7 @@ class AlphaZero:
         return masked_probs
 
     def self_play(self, num_games=1):
-      
+
       training_data = []
 
       for _ in range(num_games):
@@ -526,38 +531,38 @@ class AlphaZero:
           states, policy_targets, value_targets = [], [], []
 
           while True:
-              
+
               action_probs = self.get_action_probs(state)
 
-              
+
               action = self.choose_action(action_probs)
 
-              
+
               states.append(state)
               policy_target = np.zeros(BOARD_SIZE * BOARD_SIZE)
               policy_target[action[0] * BOARD_SIZE + action[1]] = 1
               policy_targets.append(policy_target)
 
-              
+
               winner = game.make_move(action[0], action[1])
-              
-              
+
+
               next_state = game.get_state()
 
               if winner is not None:
-                  
+
                   value = 1 if winner == game.currentPlayer else -1
                   value_targets.extend([value] * len(states))
-                  
-                  
+
+
                   flipped_states = [np.where(s == 1, -1, np.where(s == -1, 1, s)) for s in states]
                   flipped_policy_targets = [np.flip(pt) for pt in policy_targets]
-                  
-                  
+
+
                   training_data.extend(list(zip(flipped_states, flipped_policy_targets, [-v for v in value_targets])))
                   break
 
-              
+
               state = next_state
 
           training_data.extend(list(zip(states, policy_targets, value_targets)))
@@ -565,41 +570,41 @@ class AlphaZero:
       return training_data
 
     def choose_action(self, action_probs):
-        
-        
+
+
         actions = np.arange(BOARD_SIZE * BOARD_SIZE)
 
-        
+
         probs = action_probs.flatten()
 
         if np.all(probs == 0):
-            
+
             print("Warning: All probabilities are zero. Choosing a random action.")
             chosen_index = np.random.choice(actions)
         elif np.isnan(probs).any():
             print("Warning: NaN detected in probabilities. Choosing a random action.")
             chosen_index = np.random.choice(actions)
         else:
-            probs = probs / np.sum(probs)  
+            probs = probs / np.sum(probs)
             chosen_index = np.random.choice(actions, p=probs)
 
         return chosen_index // BOARD_SIZE, chosen_index % BOARD_SIZE
 
     def train(self, training_data, epochs=10, batch_size=32):
-        
+
         states, policy_targets, value_targets = zip(*training_data)
 
-        
+
         states = np.array(states)
         policy_targets = np.array(policy_targets)
         value_targets = np.array(value_targets)
 
-        
+
         self.model.fit(states, {'policy': policy_targets, 'value': value_targets},
                        epochs=epochs, batch_size=batch_size, verbose=1)
 
     def play_against_human(self, human_starts=True):
-        
+
         game = Game()
         game.initialize_board()
 
@@ -610,7 +615,7 @@ class AlphaZero:
 
             if (game.currentPlayer == PLAYER_X and human_starts) or \
                (game.currentPlayer == PLAYER_O and not human_starts):
-                
+
                 try:
                     row, col = map(int, input("Enter your move (row col): ").split())
                     if (row, col) not in game.get_valid_moves():
@@ -621,14 +626,14 @@ class AlphaZero:
                     continue
                 winner = game.make_move(row, col)
             else:
-                
+
                 state = game.get_state()
                 action_probs = self.get_action_probs(state)
                 action = np.unravel_index(np.argmax(action_probs), action_probs.shape)
                 print(f"AI moves to: ({action[0]}, {action[1]})")
                 winner = game.make_move(action[0], action[1])
 
-            
+
             if winner is not None:
                 print("Final Board:")
                 for row in game.board:
@@ -639,53 +644,107 @@ class AlphaZero:
                     print(f"Winner: {winner}")
                 break
 
+def save_training_data(training_data, filepath):
+    """
+    保存 training_data 到指定文件路径
+    """
+    with open(filepath, 'wb') as f:
+        pickle.dump(training_data, f)
+    print(f"Training data saved to {filepath}")
+
+def load_training_data(filepath):
+    """
+    从指定文件路径加载 training_data
+    """
+    with open(filepath, 'rb') as f:
+        training_data = pickle.load(f)
+    print(f"Training data loaded from {filepath}")
+    return training_data
 
 if __name__ == "__main__":
     print("1. Train AlphaZero from scratch")
     print("2. Continue training AlphaZero")
     print("3. Play against AlphaZero")
-    choice = "2"
+    choice = sys.argv[1]
 
     if choice == "1":
         num_games = 10
         num_iterations = 10
         model_save_path = './res'
-        
-        
+
+
         alphazero = AlphaZero()
         for i in range(num_iterations):
             print(f"Iteration: {i+1}/{num_iterations}")
+            print(time.time())
             training_data = alphazero.self_play(num_games)
+            print(time.time())
             alphazero.train(training_data)
+            print(time.time())
+            print()
             alphazero.save_model(model_save_path + f"_iteration_{i+1}.keras")
 
         alphazero.save_model("./res.keras")
 
     elif choice == "2":
-        num_games = 20
-        num_iterations = 20
-        model_load_path = "./aichesser-model/res.keras"
-        model_save_path = "./res"
-        
-        
+        num_games = int(input("Enter number of self-play games for each iteration: "))
+        num_iterations = int(input("Enter number of training iterations: "))
+        model_load_path = input("Enter model load path (e.g., models/alphazero_model_iteration_5): ")
+        model_save_path = input("Enter model save path (e.g., models/alphazero_model): ")
+
+
         alphazero = AlphaZero(model_path=model_load_path)
         for i in range(num_iterations):
             print(f"Iteration: {i+1}/{num_iterations}")
+            print(time.time())
             training_data = alphazero.self_play(num_games)
+            print(time.time())
             alphazero.train(training_data)
+            print(time.time())
+            print()
             alphazero.save_model(model_save_path + f"_iteration_{i+1}.keras")
-            
-        alphazero.save_model("./res.keras")
+
+        alphazero.save_model(model_save_path)
 
     elif choice == "3":
         model_load_path = input("Enter model load path (e.g., models/alphazero_model_iteration_10): ")
         human_starts = input("Do you want to start first? (y/n): ").lower() == 'y'
-        
-        
+
+
         alphazero = AlphaZero(model_path=model_load_path)
         alphazero.play_against_human(human_starts=human_starts)
 
+    elif choice == "4":
+        num_games = int(sys.argv[2])
+        # num_iterations = int(input("Enter number of training iterations: "))
+        model_load_path = sys.argv[3]
+        model_save_path = sys.argv[4]
+
+        alphazero = AlphaZero(model_path=model_load_path)
+        training_data = alphazero.self_play(num_games)
+        save_training_data(training_data, model_save_path)
+
+    elif choice == "5":
+        num_games = int(sys.argv[2])
+        num_iterations = int(sys.argv[3])
+        model_load_path = sys.argv[4]
+        model_save_path = sys.argv[5]
+
+        process_list = []
+
+        for i in range(num_iterations):
+            print(f"Iteration: {i+1}/{num_iterations}")
+            process_list.append(subprocess.Popen(["python3", "data_gen.py", "4", f"{num_games}", model_load_path, f"data{i}.pk1"]))
+
+        alphazero = AlphaZero(model_path=model_load_path)
+
+        for i in range(num_iterations):
+            process_list[i].wait()
+            training_data = load_training_data(f"data{i}.pk1")
+            alphazero.train(training_data)
+            alphazero.save_model(model_save_path + f"_iteration_{i+1}.keras")
+
+        alphazero.save_model(model_save_path)
+
     else:
         print("Invalid choice!")
-
-
